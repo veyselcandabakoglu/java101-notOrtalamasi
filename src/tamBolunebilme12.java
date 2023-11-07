9c232fb3c3a45dd10e1a93a6e81992629550805c
veyselcandabakoglu <88546719+veyselcandabakoglu@users.noreply.github.com>
2023-11-07 13:59:31 +0300

    Update tamBolunebilme12.java

--- src/tamBolunebilme12.java
+++ src/tamBolunebilme12.java
@@ -4,22 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
      
-   int k;
+   int k,total = 0, sayac= 0;
+   double ortalama;
 
    
    Scanner input = new Scanner(System.in);
    
-   System.out.println("Sayı giriniz: ");
+   System.out.println("Sayi giriniz: ");
    k= input.nextInt();
    
    for(int i=1 ; i <= k ; i++ ) {
      if((i % 3 == 0) && (i % 4 == 0)) {
-       System.out.println(i);
+
+       total += i;
+       sayac++;
+       System.out.println("Ortalama: "+ (total/sayac));
      }
    }
    
     
      }
-     
-     }
+}
   
