a644652fe44c39cddb97e906720a8022a25ed1e6
veyselcandabakoglu <88546719+veyselcandabakoglu@users.noreply.github.com>
2023-11-10 10:54:42 +0300

    asal sayılar

---
+++ src/AsalSayilar.java
@@ -0,0 +1,20 @@
+public class AsalSayilar {
+    public static void main(String[] args) {
+        System.out.println("1-100 arasındaki asal sayılar:");
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asalMi = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            if (asalMi) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
